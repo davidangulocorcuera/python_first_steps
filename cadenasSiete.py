f00b1d01f979e02df1a045727adea79064630359
David <davidangulococruera@gmail.com>
2019-01-30 19:46:29 +0100

    7 terminado

---
+++ cadenasSiete.py
@@ -0,0 +1,17 @@
+class Corcho:
+    bodega = 'San Martin De Valdeiglesias'
+
+
+class Botella:
+    corcho = Corcho()
+    isOpen = False
+
+
+class Sacacorchos:
+    def destapar(self,corcho,botella):
+        botella.isOpen = True
+        corchoSacado = corcho.bodega
+
+        
+def limpiar():
+    Botella.corcho = Corcho()
